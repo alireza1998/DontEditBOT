2c6c374f01ccc32af23836778d099d00497fa14b
alireza1998 <alireza.eidani1000@gmail.com>
2016-07-26 13:05:56 +0430

    Update Luncher.php

--- Luncher.php
+++ Luncher.php
@@ -1,5 +1,5 @@
 <?php
-define('BOT_TOKEN', '226018563:AAFqRfXKXHKeLH07Zly5clnzm0kKgQj9Az0');
+define('BOT_TOKEN', '226018563:AAFwdJZuAx9UKgtjpGyuy4vuTQ1msaV8paM');
 define('API_URL', 'https://api.telegram.org/bot'.BOT_TOKEN.'/');
 
 function apiRequestWebhook($method, $parameters) {
